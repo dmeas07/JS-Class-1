1afba0efdbc2a7eb288d8fe6098f024b5e08af5f
dmeas07 <110435805+dmeas07@users.noreply.github.com>
2022-08-22 17:29:47 -0700

    Add files via upload

---
+++ assignment 1.js
@@ -0,0 +1,53 @@
+
+let name ="Daravy";
+let email ="xxxxx@gmail.com";
+let passwords ="1234567";
+let age ="33";
+let country ="United States";
+let salary =4000;
+let annualsalary =salary*12;
+let placeofbirth ="San Diego, CA USA"
+let allergies ="none"
+let nickname ="Lavy"
+let caryear ="2006"
+let carmake ="Toyota"
+let carmodel ="4runner"
+let font ="Arial"
+let fontSize ="10px"
+let occupation ="Tattoo Artist"
+let pets ="yes"
+let typeofpets ="dogs"
+let gender ="Male"
+let degree ="No"
+let highschool ="Yes"
+
+
+document.write(`
+<p>Name: ${name}</p>
+<p>Email: ${email}</p
+<p>Passwords: ${passwords}</p>
+<p>Age: ${age}</p>
+<p>Country: ${country}</p>
+<p>Salary: ${annualsalary}</p>
+<p>Place of Birth: ${placeofbirth}</p>
+<p>Allergies: ${allergies}</p>
+<p>Nickname: ${nickname}</p>
+<p>Car Year: ${caryear}</p>
+<p>Car Make: ${carmake}</p>
+<p>Car Model: ${carmodel}</p>
+<p>Font: ${font}</p>
+<p>Font Size: ${fontSize}</p>
+<p>Occupation: ${occupation}</p>
+<p>Pets: ${pets}</p>
+<p>Gender: ${gender}</p>
+<p>Degree?: ${degree}</p>
+<p>High School?: ${highschool}</p>
+<p>Type of Pets: ${typeofpets}</p>
+
+
+
+
+
+
+
+`);
